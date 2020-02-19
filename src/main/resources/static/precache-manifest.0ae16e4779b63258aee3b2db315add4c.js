2bfbc8bf6495050dce76aa6dd314a1f4da368545
nyyppa <atte.yliverronen@gmail.com>
2020-02-19 20:41:30 +0200

    -changed frontend to create-react-app

---
+++ src/main/resources/static/precache-manifest.0ae16e4779b63258aee3b2db315add4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16da21319d80f4a4334f12360833916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ec9dff006cda8ab3b7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "175eeae142ec1d849762",
+    "url": "/static/js/2.f06f91d3.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.f06f91d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ec9dff006cda8ab3b7",
+    "url": "/static/js/main.afde2678.chunk.js"
+  },
+  {
+    "revision": "f6c374b62c3f15c2378b",
+    "url": "/static/js/runtime-main.47cd12b7.js"
+  }
+]);
